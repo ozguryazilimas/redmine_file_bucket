a2166f34192b84bfffe46046f209e2e2d4530d88
Onur Küçük <onur@delipenguen.net>
2015-06-08 23:36:36 +0300

    refs #16358 use activerecord magic to follow relations and pluck attachment ids

--- app/controllers/file_bucket_controller.rb
+++ app/controllers/file_bucket_controller.rb
@@ -5,11 +5,65 @@ class FileBucketController < ApplicationController
   before_filter :authorize
 
   def index
+    settings = RfbProjectSetting.settings_for_project(@project)
+    @container_type = settings.active_container_type
+    @attachments = prepare_attachments(@project, @container_type)
+    @include_children = settings.subproject_enabled
 
     respond_to do |format|
       format.html
-      format.json { render :json => [] }
+      format.json {
+        render :json => prepare_attachments(@project, @container_type, @include_children)
+      }
     end
+  end
+
+
+  private
+
+
+  def prepare_attachments(project, container_type, include_children = false)
+    where_arr = []
+    where_id_arr = []
+
+    container_type.each do |ct|
+      case ct
+      when 'Project'
+        where_arr << "container_type = 'Project' AND container_id IN (?)"
+
+        if include_children
+          project.self_and_descendants.ids
+        else
+          where_id_arr << [project.id]
+        end
+      when 'Issue'
+        where_arr << "container_type = 'Issue' AND container_id IN (?)"
+
+        if include_children
+          project.self_and_descendants.joins(:issues).pluck('issues.id')
+        else
+          where_id_arr << project.issue_ids
+        end
+      when 'Document'
+        where_arr << "container_type = 'Document' AND container_id IN (?)"
+
+        if include_children
+          project.self_and_descendants.joins(:documents).pluck('documents.id')
+        else
+          where_id_arr << project.document_ids
+        end
+      when 'WikiPage'
+        where_arr << "container_type = 'WikiPage' AND container_id IN (?)"
+
+        if include_children
+          project.self_and_descendants.joins(:wiki => :pages).pluck('wiki_pages.id')
+        else
+          where_id_arr << project.wiki.page_ids
+        end
+      end
+    end
+
+    Attachment.where(where_arr.join(' OR '), *where_id_arr)
   end
 
 end
--- app/models/rfb_project_setting.rb
+++ app/models/rfb_project_setting.rb
@@ -15,8 +15,10 @@ class RfbProjectSetting < ActiveRecord::Base
 
   AVAILABLE_OPTIONS = ATTACHMENT_CONTENT_TYPES + PROJECT_OPTIONS
 
+
   attr_accessible :project_id, :issue_enabled, :wiki_page_enabled, :document_enabled, :project_enabled,
     :subproject_enabled
+
 
   scope :for_project, ->(project) {
     proj_id = project.is_a?(Class) ? project.id : project
@@ -26,6 +28,19 @@ class RfbProjectSetting < ActiveRecord::Base
   scope :settings_for_project, ->(proj_id) {
     for_project(proj_id).first_or_initialize(RedmineFileBucket.settings)
   }
+
+
+  def active_container_type
+    active_list = []
+
+    AVAILABLE_OPTIONS.each do |opt|
+      symbolized = "#{opt.to_s}_enabled"
+      container_type = opt.to_s.titleize.gsub(' ', '')
+      active_list << container_type if self.send(symbolized)
+    end
+
+    active_list
+  end
 
 end
 
