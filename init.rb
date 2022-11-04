52ca9ffd9159aa78e41347f0a98d0601c637b95f
Onur Küçük <onur@delipenguen.net>
2022-11-05 01:53:58 +0300

    compatibility for both redmine 4 and 5

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'redmine'
-require 'redmine_file_bucket'
+require_dependency File.join(File.dirname(__FILE__), 'lib/redmine_file_bucket')
 
 Redmine::Plugin.register :redmine_file_bucket do
   name 'Redmine File Bucket plugin'
